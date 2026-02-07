15708b1e4728286760b1825a652adaa3c7cdcca2
NepSauce <greasyandmoist@gmail.com>
2026-02-07 01:40:12 -0400

    Build: Push to fix pipeline for missing Model file

---
+++ Models/QuickAccessItem.cs
@@ -0,0 +1,8 @@
+namespace SauceBucket.Models
+{
+    public class QuickAccessItem
+    {
+        public string Label { get; set; } = string.Empty;
+        public string Target { get; set; } = string.Empty;
+    }
+}
